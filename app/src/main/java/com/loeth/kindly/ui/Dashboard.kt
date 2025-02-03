fd40e37891f15326969e230281ffbf1ab67e3030
Apochi Ali <aliapochi@yahoo.com>
2025-02-03 18:23:32 +0100

    Dashboard complete

--- app/src/main/java/com/loeth/kindly/ui/Dashboard.kt
+++ app/src/main/java/com/loeth/kindly/ui/Dashboard.kt
@@ -2,9 +2,7 @@ package com.loeth.kindly.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,15 +13,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -32,7 +30,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -87,56 +84,305 @@ fun Dashboard() {
                 .padding(16.dp)
         )
         {
-            //Active Promises Card
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .padding(8.dp),
-                shape = RoundedCornerShape(8.dp),
-            ) {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "Active Promises",
-                        fontWeight = FontWeight.SemiBold,
-                        fontSize = 24.sp
-                    )
 
-                    Spacer(modifier = Modifier.height(16.dp))
+            ActivePromisesCard()
 
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Column(modifier = Modifier.weight(1f)) {
-                            Text(
-                                text = "00",
-                                fontWeight = FontWeight.SemiBold,
-                                fontSize = 64.sp,
-                                color = Color(0xFF4B39EF)
-                            )
-                            Text(
-                                text = "Ongoing Commitments",
-                                fontSize = 14.sp,
-                                fontWeight = FontWeight.Normal,
-                                color = Color.Gray
-                            )
-                        }
-                        Image(
-                            painter = painterResource(id = R.drawable.handshake),
-                            contentDescription = "Handshake",
-                            modifier = Modifier.size(40.dp)
-                        )
-                    }
-                }
-            }
-            //Promises due for the week Card
-            //Recent Activity Card
+            PromisesDueForTheWeekCard()
+
+            RecentActivityCard()
+
+            ImpactSummaryCard()
+
         }
 
     }
 
+}
+
+@Composable
+fun ActivePromisesCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp),
+        shape = RoundedCornerShape(8.dp),
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "Active Promises",
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = "00",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 64.sp,
+                        color = Color(0xFF4B39EF)
+                    )
+                    Text(
+                        text = "Ongoing Commitments",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = Color.Gray
+                    )
+                }
+                Image(
+                    painter = painterResource(id = R.drawable.handshake),
+                    contentDescription = "Handshake",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun PromisesDueForTheWeekCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Text(
+                    text = "Due for the Week",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = "View All",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Gray
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.dollar),
+                    contentDescription = "dollar",
+                    modifier = Modifier.size(40.dp)
+                )
+
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Monthly Support", fontWeight = FontWeight.Normal, fontSize = 16.sp)
+                    Text(
+                        text = "Due in 2 days . $250",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp,
+                        color = Color(0xFFFF5963)
+                    )
+                }
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.belief),
+                    contentDescription = "belief",
+                    modifier = Modifier.size(40.dp)
+                )
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = "Food Bank Volunteer",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Saturday . 2 hours",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp,
+                        color = Color(0xFF57636C)
+                    )
+                }
+
+            }
+        }
+
+    }
+}
+
+@Composable
+fun RecentActivityCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Text(
+                    text = "Recent Activity",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 24.sp
+                )
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = "check icon",
+                    modifier = Modifier.size(40.dp)
+                )
+
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = "Community Center Support",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Completed yesterday",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp,
+                        color = Color(0xFF249689)
+                    )
+                }
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.stopclock),
+                    contentDescription = "clock icon",
+                    modifier = Modifier.size(40.dp)
+                )
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = "Elderly care visit",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        text = "Saturday . 2 hours",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp,
+                    )
+                }
+
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ImpactSummaryCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        )
+        {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Text(
+                    text = "Impact Summary",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 24.sp
+                )
+
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                //Promises Kept
+                Column() {
+                    Text(
+                        text = "45", fontWeight = FontWeight.SemiBold,
+                        fontSize = 28.sp, color = Color(0xFF4B39EF)
+                    )
+                    Text(
+                        text = "Promises Kept",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp, color = Color (0xFF57636C)
+                    )
+                }
+                //Total Support
+                Column() {
+                    Text(
+                        text = "$2.5k", fontWeight = FontWeight.SemiBold,
+                        fontSize = 28.sp, color = Color(0xFF39D2C0)
+                    )
+                    Text(text = "Total Support", fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp, color = Color (0xFF57636C))
+                }
+                //People helped
+                Column() {
+                    Text(
+                        text = "32",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 28.sp,
+                        color = Color(0xFFEE8B60)
+                    )
+                    Text(text = "People Helped", fontWeight = FontWeight.Normal,
+                        fontSize = 12.sp, color = Color (0xFF57636C))
+                }
+
+            }
+        }
+    }
 }
 
 @Preview
